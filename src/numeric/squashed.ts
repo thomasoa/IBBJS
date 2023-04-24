df17581a097a4341707b6d8ca537ad6a09eed0a2
Thomas Andrews <github@thomasoandrews.com>
2023-04-24 16:44:16 -0400

    Add encode method to squashed.

--- src/numeric/squashed.ts
+++ src/numeric/squashed.ts
@@ -1,4 +1,12 @@
 import { choose } from "./choose.js";
 
+function encode(sortedValues: Array<number>):bigint {
+    const binomials:bigint[] = sortedValues.map(
+        (value:number, index:number):bigint => choose(value,index+1)
+    )
+    return binomials.reduce(
+        (sum:bigint,current:bigint):bigint => sum+current
+    )
+}
 
-export {choose}
+export {encode}
