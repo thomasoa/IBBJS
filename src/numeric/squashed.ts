203f38b295532177d10aeeeb6035a1d469755293
Thomas Andrews <github@thomasoandrews.com>
2023-04-24 17:25:32 -0400

    Added dummy decode method to squashed.
    Added more tests for squashed.encode

--- src/numeric/squashed.ts
+++ src/numeric/squashed.ts
@@ -1,12 +1,16 @@
 import { choose } from "./choose.js";
 
-function encode(sortedValues: Array<number>):bigint {
+function encode(sortedValues: number[]):bigint {
     const binomials:bigint[] = sortedValues.map(
         (value:number, index:number):bigint => choose(value,index+1)
     )
     return binomials.reduce(
         (sum:bigint,current:bigint):bigint => sum+current
     )
+}
+
+function decode(value:bigint, n:number): number[] {
+    return [1]
 }
 
 export {encode}
--- tests/squashed.test.js
+++ tests/squashed.test.js
@@ -1,5 +1,14 @@
 var squashed = require('../dest/numeric/squashed.js')
+var choose = require('../dest/numeric/choose.js')
 
 test('encode 0,1,2,3,4', () => {
   expect(squashed.encode([0,1,2,3,4])).toBe(BigInt(0));
+});
+
+test('encode 0,1,2,3,5', () => {
+  expect(squashed.encode([0,1,2,3,5])).toBe(BigInt(1));
+});
+
+test('encode 8,9,10', () => {
+  expect(squashed.encode([8,9,10])).toBe(choose.choose(11,3)-BigInt(1));
 });
