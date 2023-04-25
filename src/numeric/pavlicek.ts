1b725cec200512afad90639c285a96004fcfbc61
Thomas Andrews <github@thomasoandrews.com>
2023-04-25 14:26:07 -0400

    Import types to pavlicek, and add undefined as valid constructor argument.

--- src/numeric/pavlicek.ts
+++ src/numeric/pavlicek.ts
@@ -1,9 +1,12 @@
-import {DealSignature, signature_or_default, NumericDeal} from './deal.js'
+import {
+    DealSignature, signature_or_default, NumericDeal, 
+    CardNumber, HandArray, SeatNumber, PageNumber // types
+} from './deal.js'
 
 class PavlicekStrategy {
     readonly signature:DealSignature
 
-    constructor(signature:DealSignature) {
+    constructor(signature:DealSignature|undefined) {
         this.signature = signature_or_default(signature)
     }
 
