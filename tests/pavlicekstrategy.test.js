998a911e177db81167a15267d1012875c7b37ea6
Thomas Andrews <github@thomasoandrews.com>
2023-04-25 17:39:43 -0400

    Ensure deals are unique in PavlicekStrategy.

--- tests/pavlicekstrategy.test.js
+++ tests/pavlicekstrategy.test.js
@@ -25,13 +25,17 @@ test("Pavlicek strategy decode - last page", () => {
   expect(deal.toWhom).toEqual([3,3,3,3,2,2,2,1,1,0])
 })
 
-test("Andrews book complete deals [2,2,2,2]",() => {
+test("Andrews book complete deals ensure unique [2,2,2,2]",() => {
   var sig = new numDeal.DealSignature([2,2,2,2])
   var pBook = new pavlicek.PavlicekStrategy(sig)
-
+  var map = new Map()
   for (var page=BigInt(0); page<sig.pages; page++) {
     var deal = pBook.computePageContent(page)
-    expect(deal).toBeDefined()
+
+    var dealString = deal.toWhom.join('')
+    
+    expect(map.has(dealString)).toBeFalsy()
+    map.set(dealString,page)
   }
 })
 
