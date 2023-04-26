2ded89e105f94d9d7c4a11cb3f950132d2672ed7
Thomas Andrews <github@thomasoandrews.com>
2023-04-26 12:52:16 -0400

    Add test for scrambled book strategy.

--- tests/scramble.test.js
+++ tests/scramble.test.js
@@ -1,5 +1,6 @@
-import {MultiplierScrambler, ScrambleStrategy} from '../dest/numeric/scramble.js'
+import {MultiplierScrambler, scramble_book} from '../dest/numeric/scramble.js'
 import {AndrewsStrategy} from '../dest/numeric/andrews.js'
+import { DealSignature } from '../dest/numeric/deal.js'
 
 test('Multiplier scrambler',() => {
     var modulus = BigInt(11)
@@ -12,3 +13,14 @@ test('Multiplier scrambler',() => {
         expect(scrambler.unscramble(scrambler.scramble(i))).toBe(i)
     }
 })
+
+test("Scrambled book complete invertible for signature [2,2,2,2]",() => {
+    var sig = new DealSignature([2,2,2,2])
+    var aBook = new AndrewsStrategy(sig)
+    var sBook = scramble_book(aBook,BigInt(11*17*31),BigInt(1001))
+ 
+    for (var page=BigInt(0); page<sig.pages; page++) {
+      var deal = sBook.computePageContent(page)
+      expect(sBook.computePageNumber(deal)).toBe(page)
+    }
+ })
