d50987b48e661f127baf650d931f8f40260f7d39
Thomas Andrews <github@thomasoandrews.com>
2023-04-26 18:20:27 -0400

    Include NumericDeal in exports.

--- src/numeric/book.ts
+++ src/numeric/book.ts
@@ -1,6 +1,6 @@
 //  Just a single source library for all you need to import
 // from this numeric directory
-import {DealSignature, BookStrategy} from "./deal.js"
+import {DealSignature, BookStrategy, NumericDeal} from "./deal.js"
 import {AndrewsStrategy} from "./andrews.js"
 import { PavlicekStrategy } from "./pavlicek.js"
 import { scramble_book } from "./scramble.js"
@@ -10,4 +10,4 @@ import { scramble_book } from "./scramble.js"
 
 
 
-export {DealSignature, AndrewsStrategy, PavlicekStrategy, BookStrategy, scramble_book}
+export {DealSignature, AndrewsStrategy, PavlicekStrategy, BookStrategy, NumericDeal, scramble_book}
