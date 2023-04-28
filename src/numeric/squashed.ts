43bc0287aaa24e6406e9290525cccacb1c3b9bce
Thomas Andrews <github@thomasoandrews.com>
2023-04-28 13:17:09 -0400

    Add comments.

--- src/numeric/squashed.ts
+++ src/numeric/squashed.ts
@@ -1,6 +1,25 @@
 import { choose } from "./choose.js";
 
+/**
+ * The squashed order is a total order on all sets of n
+ * natural number. For example, when n=3, the order start:
+ *     012,013,023,123,014,024,034,124,134,234,...
+ * 
+ * The order has a convenient feature that we can find the index
+ * of a set in this order just from the set.
+ */
+
+/**
+ * Compute the index in the squashed order for a set of values
+ * 
+ * @param sortedValues - a set of sorted values
+ * @returns bigint index of the set of values in the squashed order
+ * 
+ * Assumes the sortedValues are sorted and distinct - nothing
+ * ensures this.
+ */
 function encode(sortedValues: readonly number[]):bigint {
+    
     const binomials:bigint[] = sortedValues.map(
         (value:number, index:number):bigint => choose(value,index+1)
     )
@@ -9,6 +28,13 @@ function encode(sortedValues: readonly number[]):bigint {
     )
 }
 
+/**
+ * Find the largest value k such that (k chooose n)<index
+ * 
+ * @param index 
+ * @param n 
+ * @returns The largest value k
+ */
 function largestLessThan(index:bigint, n:number): number {
     var k:number = n-1
     while (choose(k+1,n)<=index) {
@@ -17,6 +43,13 @@ function largestLessThan(index:bigint, n:number): number {
     return k
 }
 
+/**
+ * Find the set of size n of the given index in the squashed order.
+ * 
+ * @param index - the index
+ * @param n - the size of the sets
+ * @returns a sorted array of distinct natural numbers
+ */
 function decode(index:bigint, n:number): number[] {
     // Compute the n-subset of the natural numbers with the
     // given index.
