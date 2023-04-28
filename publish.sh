1577f7211ee687555e02af70eebde211a6038dc2
Thomas Andrews <github@thomasoandrews.com>
2023-04-28 07:30:54 -0400

    Updates

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-( cd dest ; tar cf - .) | (cd thomasoa.github.io/impossible; tar xvf -)
-cd thomasoa.gitgub.io
+( cd dest ; tar cf - .) | (cd ../thomasoa.github.io/impossible; tar xvf -)
+cd ../thomasoa.gitgub.io
 git add .
 git commit -m $1
 git push
