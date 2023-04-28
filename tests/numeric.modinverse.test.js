ff3e439917eb675e29cb6accba3354e50d0f322e
Thomas Andrews <github@thomasoandrews.com>
2023-04-28 12:24:58 -0400

    Update test to include negative number examples.

--- tests/numeric.modinverse.test.js
+++ tests/numeric.modinverse.test.js
@@ -1,5 +1,20 @@
-var mod_inverse = require( '../dest/numeric/modinverse.js').modular_inverse
-var int_mod_inverse = (m,u) => mod_inverse(BigInt(m),BigInt(u))
+import {modular_inverse,safe_mod, long_gcd} from '../dest/numeric/modinverse.js'
+
+function int_mod_inverse(m,n) {
+    var value = modular_inverse(BigInt(m),BigInt(n))
+    //console.log('int_mod_inverse',m,n,value)
+    return value
+}
+
+function int_safe_mod(m,n) {
+    return safe_mod(BigInt(m),BigInt(n))
+}
+
+test('safe_mod when normal mod might return negative',()=>{
+   expect(int_safe_mod(13,-3)).toBe(BigInt(1))
+   expect(int_safe_mod(-13,3)).toBe(BigInt(2))
+   expect(int_safe_mod(-13,-3)).toBe(BigInt(2))
+})
 
 test('Inverse of 3 modulo 7',() => {
     expect(int_mod_inverse(7,3)).toBe(BigInt(5))
@@ -15,4 +30,12 @@ test('Expect throw when unit is not relatively prime to modulus', () => {
 
 test('Expect inverse of 34 modulo 55 is 34', () => {
     expect(int_mod_inverse(55,34)).toEqual(BigInt(34))
+})
+
+test('Find gcd of 53644737765488792839237440000 and unit 13109994191499930367061460371',()=>{
+    var m = BigInt('53644737765488792839237440000')
+    var n = BigInt('13109994191499930367061460371')
+    expect(long_gcd(m,n).gcd).toBe(BigInt(1))
+    var inverse = modular_inverse(m,n)
+    expect((inverse*n)%m).toBe(BigInt(1))
 })
--- tests/numeric.scramble.test.js
+++ tests/numeric.scramble.test.js
@@ -6,10 +6,10 @@ test('Multiplier scrambler',() => {
     var modulus = BigInt(11)
     var multiplier = BigInt(3)
     var translate = BigInt(7)
+    var scrambler = new MultiplierScrambler(modulus,multiplier,translate)
+
     expect(scrambler.scramble(BigInt(1))).toBe(BigInt((1*3+7)%11))
     expect(scrambler.scramble(BigInt(2))).toBe(BigInt((2*3+7)%11))
-
-    var scrambler = new MultiplierScrambler(modulus,multiplier,translate)
     expect(scrambler.scramble(BigInt(0))).toBe(BigInt(7))
 
     for (var i=BigInt(0); i<modulus; i++) {
@@ -23,7 +23,7 @@ test('Multiplier with negative values',()=>{
     var translate = BigInt(-7)
     var scrambler = new MultiplierScrambler(modulus,multiplier,translate)
     expect(scrambler.scramble(BigInt(1))).toBe(BigInt((1*(11-3)+(11-7))%11))
-    expect(scrambler.scramble(BigInt(2))).toBe(BigInt((1*(11-3)+(11-7))%11))
+    expect(scrambler.scramble(BigInt(2))).toBe(BigInt((2*(11-3)+(11-7))%11))
     for (var i=BigInt(0); i<modulus; i++) {
         expect(scrambler.unscramble(scrambler.scramble(i))).toBe(i)
     }
