77a9770fdfa80b07a8874d8e8263e14253e3c774
Thomas Andrews <github@thomasoandrews.com>
2023-04-30 19:33:56 -0400

    Import only the needed things from numeric.

--- src/model/books.ts
+++ src/model/books.ts
@@ -1,11 +1,16 @@
 import {BridgeBook, SeatMap} from "../bridge/book.js"
 import {Seats} from "../bridge/constants.js"
-import * as numeric from "../numeric/index.js"
+import {
+    BookStrategy, 
+    AndrewsStrategy, 
+    PavlicekStrategy, 
+    scramble_book
+}  from "../numeric/index.js"
 
-function scramble(strategy:numeric.BookStrategy):numeric.BookStrategy {
+function scramble(strategy:BookStrategy):BookStrategy {
     var multiplier = BigInt("13109994191499930367061460371")
     var translation = BigInt("34563463456363563565356345634")
-    return numeric.scramble_book(strategy,multiplier,translation)
+    return scramble_book(strategy,multiplier,translation)
 }
 
 interface Edition { 
@@ -14,18 +19,18 @@ interface Edition {
 }
 
 function edition(book:BridgeBook):Edition {
-    var scrambledStrat:numeric.BookStrategy = scramble(book.strategy)
+    var scrambledStrat:BookStrategy = scramble(book.strategy)
     var scrambled = new BridgeBook(scrambledStrat, book.seatMap, book.cardMap)
     return {book: book, scrambled: scrambled }
 }
 
 function pavlicekBook():BridgeBook {
-    var strategy = new numeric.PavlicekStrategy(undefined)
+    var strategy = new PavlicekStrategy(undefined)
     return new BridgeBook(strategy,undefined,undefined)
 }
 
 function andrewsBook():BridgeBook {
-    var strategy = new numeric.AndrewsStrategy(undefined)
+    var strategy = new AndrewsStrategy(undefined)
     var seatMap:SeatMap = (seatNumber) => Seats.all[3-seatNumber]
     return new BridgeBook(strategy,seatMap,undefined)
 }
