8ad477dbd8ac8a8d3c2348fa9bbb7cd3403236de
Thomas Andrews <github@thomasoandrews.com>
2023-05-01 21:13:49 -0400

    Single import source for bridge package.

---
+++ src/bridge/index.ts
@@ -0,0 +1,9 @@
+import {Seats,Suits, Ranks,Cards, Seat, Suit, Rank, Card} from "./constants.js"
+import  {BridgeBook, SeatMap, CardMap} from "./book.js"
+import {Deal, Hand, Holding} from "./deal.js"
+
+export {
+    Seats,Suits, Ranks,Cards, Seat, Suit, Rank, Card,
+    BridgeBook, SeatMap, CardMap,
+    Deal, Hand, Holding
+}
