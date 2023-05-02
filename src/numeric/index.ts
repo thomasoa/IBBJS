d07e5ba2bc1f97b1fc9be4c5df019662821d9184
Thomas Andrews <github@thomasoandrews.com>
2023-05-01 22:20:47 -0400

    Fix indents.

--- src/numeric/index.ts
+++ src/numeric/index.ts
@@ -1,15 +1,18 @@
 //  Just a single source entry point for all you need to 
 // the numeric directory for the Impossible Bridge Book.
 import {DealSignature, BookStrategy, NumericDeal, 
-    PageNumber,CardNumber,SeatNumber} from "./deal.js"
-    import {AndrewsStrategy} from "./andrews.js"
-    import { PavlicekStrategy } from "./pavlicek.js"
-    import { scramble_book } from "./scramble.js"
-    
-    export {
-        DealSignature, NumericDeal, 
-        AndrewsStrategy, PavlicekStrategy, // Classes
-        BookStrategy,  // interface
-        PageNumber,CardNumber,SeatNumber, // Types
-        scramble_book  // Function
-    }
+    PageNumber,CardNumber,SeatNumber,
+    bridgeSignature 
+} from "./deal.js"
+import {AndrewsStrategy} from "./andrews.js"
+import { PavlicekStrategy } from "./pavlicek.js"
+import { scramble_book } from "./scramble.js"
+
+export {
+    DealSignature, NumericDeal, 
+    AndrewsStrategy, PavlicekStrategy, // Classes
+    BookStrategy,  // interface
+    PageNumber,CardNumber,SeatNumber, // Types
+    scramble_book, // Function
+    bridgeSignature
+}
