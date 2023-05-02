61e3c4dc67f2e60e7ea4259c4d53cee9c3197b48
Thomas Andrews <github@thomasoandrews.com>
2023-05-02 12:47:49 -0400

    Update UI to have first page and last page links.

--- dest/ui/appui.mjs
+++ dest/ui/appui.mjs
@@ -1,5 +1,6 @@
 
 import { Application } from "../model/application.js"
+
 var App = new Application()
 
 function backDeal() {
@@ -12,6 +13,15 @@ function fowardDeal() {
   return false;
 }
 
+function firstDeal() {
+  App.chooseCurrent(0)
+  return false
+}
+
+function lastDeal() {
+  App.chooseCurrent(App.length-1)
+}
+
 function visibility(flag) {
   return flag ? 'visible' : 'hidden'
 }
@@ -21,67 +31,79 @@ function setVisibility(selector,flag) {
 }
 
 function updatePreviousNext() {
-  setVisibility('#back',App.allowPrevious)
-  setVisibility('#forward',App.allowNext)
+  setVisibility('#backwards',App.allowPrevious)
+  setVisibility('#forwards',App.allowNext)
 }
 
-function updateCurrentDeal(dealInfo) {
+function getTitle(dealInfo) {
+  var title = dealInfo.edition + " Edition"
+  if (dealInfo.scrambled) {
+    title = "Scrambled "+ title
+  }
+  return title
+}
 
-    var dealLoc = $('#deal')
-    if (dealInfo == undefined) {
-      dealLoc.hide()
-      return
-    } else {
-      $('#dealIndex').text(dealInfo.index+1)
-      var title = dealInfo.edition + " Edition"
-      if (dealInfo.scrambled) {
-        title = "Scrambled "+ title
-      }
-      $('#bookTitle').text(title)
-      $('#error').hide()
-      $('#pageNumber').text(dealInfo.pageNo)
-      updatePreviousNext()
-      dealInfo.deal.eachHand((seat,hand) => {
-        var handDiv = $('.diagram .'+seat.name)
-        hand.eachSuit((suit,holding) => {
-          var hString = holding.toString()
-          if (hString=='-')  { hString = '\u2014' }
-          handDiv.find('.'+suit.name+ ' span.holding').text(hString)
-        })
-      })
-      // var hands = dealInfo.deal.hands.map((hand) => hand.toString()).join("\n")
-      dealLoc.show()
-    }
+function updateDeal(deal) {
+  deal.eachHand((seat,hand) => {
+    var handDiv = $('.diagram .'+seat.name)
+    hand.eachSuit((suit,holding) => {
+      var hString = holding.toString()
+      if (hString=='-')  { hString = '\u2014' } // emdash 
+      handDiv.find('.'+suit.name+ ' span.holding').text(hString)
+    })
+  })
+}
+function updateCurrentDeal(dealInfo) {
+  
+  var dealLoc = $('#deal')
+  if (dealInfo == undefined) {
+    dealLoc.hide()
+    return
+  } 
+
+  $('#dealIndex').text(dealInfo.index+1)
+  var title = getTitle(dealInfo)
+  $('#bookTitle').text(title)
+  $('#error').hide()
+  $('#pageNumber').text(dealInfo.pageNo)
+  updatePreviousNext()
+  updateDeal(dealInfo.deal)
+  // var hands = dealInfo.deal.hands.map((hand) => hand.toString()).join("\n")
+  dealLoc.show()
+  
 }
 
 function updateDealCount(count) {
   $('#dealCount').text(count)
   updatePreviousNext()
 }
-      
+
 function reset() {
   App.reset()
-    $('#lookup').find('input[name="pageNumbers"]').val('')
-
+  
+  $('#lookup').find('input[name="pageNumbers"]').val('')
 }
 function initialize() {
-    App.listenCurrentDeal(updateCurrentDeal)
-    App.listenDealCount(updateDealCount)
-    const form = $('#lookup')
-    form.submit(() => {
-        submit_pages(form)
-        return false;
-    }) 
-    $('#reset').on('click',() => reset())
-    $('#back').on('click',() => backDeal())
-    $('#forward').on('click',() => fowardDeal())
-    App.reset()
+  App.listenCurrentDeal(updateCurrentDeal)
+  App.listenDealCount(updateDealCount)
+  const form = $('#lookup')
+  form.submit(() => {
+    submit_pages(form)
+    return false;
+  }) 
+  $('#reset').on('click',() => reset())
+  $('#firstDeal').on('click',() => firstDeal())
+  $('#lastDeal').on('click',() => lastDeal())
+
+  $('#back').on('click',() => backDeal())
+  $('#forward').on('click',() => fowardDeal())
+  App.reset()
 }
 
 function submitPages(form,pageNumbers) {
   var scramble = form.find('select[name="scramble"]').val()
-    var edition = form.find('select[name="edition"]').val()
-    App.findDeals(edition,scramble=='Scrambled',pageNumbers)
+  var edition = form.find('select[name="edition"]').val()
+  App.findDeals(edition,scramble=='Scrambled',pageNumbers)
 }
 
 function submit_pages(form) {
@@ -92,11 +114,11 @@ function submit_pages(form) {
     submitPages(form,pages)
     $(pageEntry).val('')
     $('#error').hide() } catch (e) {
-    console.error(e)
-    $('#error').text(e)
-    $('#error').show()
-    return false
+      console.error(e)
+      $('#error').text(e)
+      $('#error').show()
+      return false
+    }
   }
-}
-
-export {initialize, App}
+  
+  export {initialize, App}
