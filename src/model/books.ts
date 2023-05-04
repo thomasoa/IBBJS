0a8862562d7ae4caa5a75ea588e7403d8d1a5321
Thomas Andrews <github@thomasoandrews.com>
2023-05-04 19:23:56 -0400

    Code reformat.

--- src/model/books.ts
+++ src/model/books.ts
@@ -1,107 +1,107 @@
-import {BridgeBook,SimpleBijection ,Deal, Seat, Seats} from "../bridge/index.js"
+import { BridgeBook, SimpleBijection, Deal, Seat, Seats } from "../bridge/index.js"
 import {
-    BookStrategy, 
-    AndrewsStrategy, 
-    PavlicekStrategy, 
+    BookStrategy,
+    AndrewsStrategy,
+    PavlicekStrategy,
     scramble_book,
     MultiplierScrambler,
     Scrambler,
     bridgeSignature,
     ScrambleStrategy,
     PageNumber
-}  from "../numeric/index.js"
+} from "../numeric/index.js"
 
-function common_scrambler():Scrambler {
+function common_scrambler(): Scrambler {
     const multiplier = BigInt("13109994191499930367061460371")
     const translation = BigInt("34563463456363563565356345634")
-    return new MultiplierScrambler(bridgeSignature.pages,multiplier,translation)
+    return new MultiplierScrambler(bridgeSignature.pages, multiplier, translation)
 }
 
-interface Edition { 
-    normal: BridgeBook, 
-    scrambled: BridgeBook 
+interface Edition {
+    normal: BridgeBook,
+    scrambled: BridgeBook
 }
 
-function edition(book:BridgeBook,scrambler:Scrambler):Edition {
-    const scrambledStrat:BookStrategy = new ScrambleStrategy(book.strategy,scrambler)
+function edition(book: BridgeBook, scrambler: Scrambler): Edition {
+    const scrambledStrat: BookStrategy = new ScrambleStrategy(book.strategy, scrambler)
     const scrambled = new BridgeBook(scrambledStrat, book.seatBijection, book.cardBijection)
-    return {normal: book, scrambled: scrambled }
+    return { normal: book, scrambled: scrambled }
 }
 
-function pavlicekBook():BridgeBook {
+function pavlicekBook(): BridgeBook {
     const strategy = new PavlicekStrategy()
     return new BridgeBook(strategy)
 }
 
-function andrewsBook():BridgeBook {
+function andrewsBook(): BridgeBook {
     // We use a seat map to match the original book
     const strategy = new AndrewsStrategy()
-    const seatBijection = new SimpleBijection<Seat>(Seats.all, (seatNumber) => 3-seatNumber)
-    
-    return new BridgeBook(strategy,seatBijection)
+    const seatBijection = new SimpleBijection<Seat>(Seats.all, (seatNumber) => 3 - seatNumber)
+
+    return new BridgeBook(strategy, seatBijection)
 }
 
-function build_editions(scrambler:Scrambler):Map<string,Edition> {
-    const editions = new Map<string,Edition>()
-    editions.set("Pavlicek",edition(pavlicekBook(),scrambler))
-    editions.set("Andrews",edition(andrewsBook(),scrambler))
+function build_editions(scrambler: Scrambler): Map<string, Edition> {
+    const editions = new Map<string, Edition>()
+    editions.set("Pavlicek", edition(pavlicekBook(), scrambler))
+    editions.set("Andrews", edition(andrewsBook(), scrambler))
     return editions
 }
 
-type EditionPage =  {
-    name:string,
-    normal:PageNumber,
-    scrambled:PageNumber
+type EditionPage = {
+    name: string,
+    normal: PageNumber,
+    scrambled: PageNumber
 }
 
 class BookSet {
-    editions: Map<string,Edition>
+    editions: Map<string, Edition>
     scrambler: Scrambler
-    
+
     constructor() {
         this.scrambler = common_scrambler()
         this.editions = build_editions(this.scrambler)
     }
-    
-    names():string[] {
+
+    names(): string[] {
         return Array.from(this.editions.keys())
     }
 
-    edition(name:string):Edition {
-        const edition:Edition|undefined = this.editions.get(name)
+    edition(name: string): Edition {
+        const edition: Edition | undefined = this.editions.get(name)
         if (edition) return edition
-        throw new Error('Invalid edition name: '+name)
+        throw new Error('Invalid edition name: ' + name)
     }
-    
-    book(name:string, scrambled:boolean=false):BridgeBook {
-        const edition:Edition = this.edition(name)
+
+    book(name: string, scrambled: boolean = false): BridgeBook {
+        const edition: Edition = this.edition(name)
 
         if (scrambled) {
             return edition.scrambled
         } else {
-                return edition.normal
+            return edition.normal
         }
     }
 
-    unscramble(pageNo:PageNumber):PageNumber {
+    unscramble(pageNo: PageNumber): PageNumber {
         // Scrambler uses page numbers strting at zero
         const one = BigInt(1)
-        return this.scrambler.unscramble(pageNo-one)+one
+        return this.scrambler.unscramble(pageNo - one) + one
     }
 
-    pageNumbers(deal:Deal):EditionPage[] {
+    pageNumbers(deal: Deal): EditionPage[] {
         const pages = new Array<EditionPage>()
-        this.editions.forEach((edition,name)=>{
+        this.editions.forEach((edition, name) => {
             const normalPage = edition.normal.getPageNumber(deal)
             const scramblePage = this.unscramble(normalPage)
-            pages.push({name: name, normal:normalPage, scrambled: scramblePage})
+            pages.push({ name: name, normal: normalPage, scrambled: scramblePage })
         })
         return pages
     }
 
-    get lastPage():PageNumber {
+    get lastPage(): PageNumber {
         return bridgeSignature.pages
     }
 }
 
-export {BookSet}
+export { BookSet }
