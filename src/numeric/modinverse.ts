5a53d8b098a669a62de3e452ba8a6c8333553825
Thomas Andrews <github@thomasoandrews.com>
2023-05-04 14:33:38 -0400

    Greatly simplify invere method.

--- src/numeric/modinverse.ts
+++ src/numeric/modinverse.ts
@@ -4,14 +4,21 @@ interface LongGCDResult {
     quotients:bigint[]
 }
 
+const zero = BigInt(0)
+const one = BigInt(1)
+
+function abs(value:bigint):bigint {
+    if (value<BigInt(0)) {
+        return -value
+    } else {
+        return value
+    }
+}
 export function safe_mod(n1:bigint,n2:bigint): bigint {
     // Computes n1 % n2, but with values r in range
     // 0 <= r < abs(n2)
     // The % operator sometimes returns negative numbers
-    const zero = BigInt(0)
-    if (n2<zero) {
-        n2 = -n2
-    }
+    n2 = abs(n2)
     const result = n1%n2
     if (result < zero) {
         return result+n2
@@ -24,10 +31,7 @@ export function safe_mod(n1:bigint,n2:bigint): bigint {
 export function long_gcd(m:bigint,n:bigint):LongGCDResult {
     
     const quotients = Array<bigint>()
-    const zero = BigInt(0)
-    if (m<zero) {
-        m = -m
-    }
+    m = abs(m)
     
     if (m==zero) {
         throw Error('long_gcd cannot be called when m is zero')
@@ -38,43 +42,40 @@ export function long_gcd(m:bigint,n:bigint):LongGCDResult {
     while (n!=zero) {
         const q = m/n
         const r = m % n
-        if (r==zero) {
-            return {gcd: n, quotients: quotients}
+        if (r!=zero) {
+            quotients.push(q)
         }
-        quotients.push(q)
         m = n
         n = r
     }
     return {gcd: m, quotients: quotients}
 }
 
-export function modular_inverse(modulus:bigint, unit:bigint):bigint {
-    const zero = BigInt(0), one = BigInt(1)
-    
+function buildInverse(quotients:bigint[],modulus:bigint):bigint {
+    // Standard algorithm for contiued fraction expansion
+    // Numerators only needed.
+    let p_0=zero, p_1 = one
+    quotients.forEach((quotient)=>{
+        const p_new = p_1*quotient + p_0
+        p_0=p_1 ; p_1=p_new; 
+    })
+    if (quotients.length % 2 == 0) {
+        return p_1
+    } else {
+        return modulus-p_1
+    } 
+}
+
+export function modular_inverse(modulus:bigint, unit:bigint):bigint {    
     if (modulus<zero) {
         modulus = -modulus
     }
     unit = safe_mod(unit, modulus)
-    
-    if (unit == zero) {
-        throw Error('Unit ' + unit + ' is divible by the modulus '+modulus)
-    }
     const result = long_gcd(modulus,unit)
     if (result.gcd != one) {
         throw Error('Modulus ' + modulus
                     + ' and unit '+ unit 
                     + ' are not relatively prime, gcd='+result.gcd)
     }
-    let p_0=zero, p_1 = one, q_0 = one, q_1= zero
-    result.quotients.forEach((quotient)=>{
-        const p_new = p_1*quotient + p_0
-        const q_new = q_1*quotient + q_0
-        p_0=p_1 ; p_1=p_new; q_0=q_1; q_1=q_new
-    })
-    if (result.quotients.length % 2 == 0) {
-        return p_1
-    } else {
-        return modulus-p_1
-    }
-    
+    return buildInverse(result.quotients,modulus)
 }
