f6916cbf6e2329303c050900ecaa1afb15f971b8
Thomas Andrews <github@thomasoandrews.com>
2023-05-05 19:23:50 -0400

    More tests for handbook.

--- tests/bridge.handbook.test.js
+++ tests/bridge.handbook.test.js
@@ -1,11 +1,17 @@
-import {HandBook, AndrewsHandBook, PavlicekHandBook} from '../dest/bridge/handbook.js'
+import {AndrewsHandBook, PavlicekHandBook} from '../dest/bridge/handbook.js'
 
 test('First page is the same', ()=>{
     const aBook = new AndrewsHandBook()
     const pBook = new PavlicekHandBook()
+    expect(aBook.pages).toBe(pBook.pages)
+    expect(aBook.lastPage).toBe(pBook.lastPage)
+
+    const lastPage = aBook.lastPage
+
     expect(aBook.getHand(BigInt(1)).toString()).toBe('AKQJ1098765432 - - -')
-    expect(aBook.getHand(aBook.lastPage).toString()).toBe('- - - AKQJ1098765432')
+    expect(aBook.getHand(lastPage).toString()).toBe('- - - AKQJ1098765432')
     expect(pBook.getHand(BigInt(1)).toString()).toBe('AKQJ1098765432 - - -')
-    expect(pBook.getHand(pBook.lastPage).toString()).toBe('- - - AKQJ1098765432')
+    expect(pBook.getHand(lastPage).toString()).toBe('- - - AKQJ1098765432')
 
 })
+
