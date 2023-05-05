06138880cb67eaeed4223f8f680345d23c879851
Thomas Andrews <github@thomasoandrews.com>
2023-05-05 18:30:52 -0400

    Separate out the bijection class.

---
+++ src/bridge/bijection.ts
@@ -0,0 +1,38 @@
+import {Deck, Seats, Seat, Card}  from "./constants.js"
+
+interface OrderedType {
+    order:number
+}
+
+interface Bijection<T extends OrderedType> {
+    mapTo(num:number):T
+    mapFrom(t:T):number
+}
+
+class SimpleBijection<T extends OrderedType> {
+    instances: readonly T[]
+    reverse: readonly number[]
+
+    constructor(allT:readonly T[], map:(n:number)=> number = (n)=> n) {
+        const instances = new Array<T>(allT.length)
+        const reverse = new Array<number>(allT.length)
+        allT.forEach((t:T,num)=>{
+            instances[num]=allT[map(num)]
+            reverse[map(num)] = num
+        })
+        this.instances = instances
+        this.reverse = reverse
+    }
+
+    mapTo(num:number):T {
+        return this.instances[num]
+    }
+
+    mapFrom(t:T):number {
+        return this.reverse[t.order]
+    }
+}
+const defaultBijectionSeat:Bijection<Seat> = new SimpleBijection<Seat>(Seats.all)
+const defaultBijectionCard:Bijection<Card> = new SimpleBijection<Card>(Deck.cards)
+
+export {Bijection, SimpleBijection, defaultBijectionCard, defaultBijectionSeat}
