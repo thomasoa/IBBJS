aff0958fe02cc52590437e7c50a4b4a5cf2ede31
Thomas Andrews <github@thomasoandrews.com>
2023-05-05 18:51:09 -0400

    Add last page test.

--- tests/bridge.handbook.test.js
+++ tests/bridge.handbook.test.js
@@ -4,6 +4,8 @@ test('First page is the same', ()=>{
     const aBook = new AndrewsHandBook()
     const pBook = new PavlicekHandBook()
     expect(aBook.getHand(BigInt(1)).toString()).toBe('AKQJ1098765432 - - -')
+    expect(aBook.getHand(aBook.lastPage).toString()).toBe('- - - AKQJ1098765432')
     expect(pBook.getHand(BigInt(1)).toString()).toBe('AKQJ1098765432 - - -')
+    expect(pBook.getHand(pBook.lastPage).toString()).toBe('- - - AKQJ1098765432')
 
 })
