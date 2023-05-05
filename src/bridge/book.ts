b84d24725ade31384d1acfbfe6a795c263aaf8de
Thomas Andrews <github@thomasoandrews.com>
2023-05-05 18:32:27 -0400

    Remove bijections from bridge/book.ts

--- src/bridge/book.ts
+++ src/bridge/book.ts
@@ -1,41 +1,11 @@
-import {Deck, Seats, Seat, Card}  from "./constants.js"
+import {Deck, Seat, Card}  from "./constants.js"
 import {BookStrategy, PageNumber, SeatNumber,  NumericDeal, DealSignature, bridgeSignature} from "../numeric/index.js"
+import {Bijection, SimpleBijection, defaultBijectionCard, defaultBijectionSeat} from './bijection.js'
 import {Deal} from "./deal.js"
 
 interface OrderedType {
     order:number
 }
-
-interface Bijection<T extends OrderedType> {
-    mapTo(num:number):T
-    mapFrom(t:T):number
-}
-
-class SimpleBijection<T extends OrderedType> {
-    instances: readonly T[]
-    reverse: readonly number[]
-
-    constructor(allT:readonly T[], map:(n:number)=> number = (n)=> n) {
-        const instances = new Array<T>(allT.length)
-        const reverse = new Array<number>(allT.length)
-        allT.forEach((t:T,num)=>{
-            instances[num]=allT[map(num)]
-            reverse[map(num)] = num
-        })
-        this.instances = instances
-        this.reverse = reverse
-    }
-
-    mapTo(num:number):T {
-        return this.instances[num]
-    }
-
-    mapFrom(t:T):number {
-        return this.reverse[t.order]
-    }
-}
-const defaultBijectionSeat:Bijection<Seat> = new SimpleBijection<Seat>(Seats.all)
-const defaultBijectionCard:Bijection<Card> = new SimpleBijection<Card>(Deck.cards)
 
 function validate_signature(signature:DealSignature):void {
     if (!bridgeSignature.equals(signature)) {
@@ -101,5 +71,6 @@ class BridgeBook {
             return this.strategy.computePageNumber(numericDeal)+BigInt(1)
         }
     }
-    
+
+
     export {BridgeBook, SimpleBijection, validate_signature}
