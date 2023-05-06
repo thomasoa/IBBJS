e326d4aa83204ef9ca78bb412c523ef63172e8b5
Thomas Andrews <github@thomasoandrews.com>
2023-05-06 14:10:29 -0400

    Add branch to revision.json

--- publish.sh
+++ publish.sh
@@ -2,8 +2,11 @@
 
 repo=$(git config --get remote.origin.url)
 revision=$(git rev-parse HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo "{\"repo\":\"$repo\",\"branch\":\"$branch\",\"revision\":\"$revision\"}" > dest/revision.json
+exit 0
+
 [ -d ../thomasoa.github.io ] || exit 0
-echo "{\"repo\":\"$repo\",\"revision\":\"$revision\"}" > dest/revision.json
 ( cd dest ; tar cf - .) | (cd ../thomasoa.github.io/impossible; tar xvf -)
 cd ../thomasoa.github.io
 git add .
