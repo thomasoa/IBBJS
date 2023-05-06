5285baa82812600f2dec3dda4d1bf3e1e0627328
Thomas Andrews <github@thomasoandrews.com>
2023-05-06 14:35:47 -0400

    Add code to page to show revision

--- publish.sh
+++ publish.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-repo=$(git config --get remote.origin.url)
-repoURL=$(echo $repo | perl -pe 's/\.git$//;')
-revision=$(git rev-parse HEAD)
-branch=$(git rev-parse --abbrev-ref HEAD)
-url="$repoURL/commit/$revision"
-echo "{\"repo\":\"$repo\",\"branch\":\"$branch\",\"revision\":\"$url\"}" > dest/revision.json
 [ -d ../thomasoa.github.io ] || exit 0
 ( cd dest ; tar cf - .) | (cd ../thomasoa.github.io/impossible; tar xvf -)
 cd ../thomasoa.github.io
 git add .
-git commit -m "$*"
+git commit -m "IBBJS: $*"
 git push
 
