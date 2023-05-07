f1d5e9f23ee37b5eb728980d91c38e8536ebb658
Thomas Andrews <github@thomasoandrews.com>
2023-05-06 22:04:08 -0400

    Update tests to use import rather than require.

--- tests/numeric.choose.test.js
+++ tests/numeric.choose.test.js
@@ -1,4 +1,4 @@
-var choose = require('../dest/numeric/choose.js')
+import * as choose from '../dest/numeric/choose.js'
 
 test('10 choose 5 = 252', () => {
   expect(choose.choose(10, 5)).toBe(BigInt(252));
