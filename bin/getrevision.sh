8b1b55249f63116961d542270c8cb6e3b11c082f
Thomas Andrews <github@thomasoandrews.com>
2023-05-11 18:34:46 -0400

    Add date to revision file and put it on the page.

--- bin/getrevision.sh
+++ bin/getrevision.sh
@@ -5,5 +5,6 @@ repoURL=$(echo $repo | perl -pe 's/\.git$//;')
 revision=$(git rev-parse HEAD)
 branch=$(git rev-parse --abbrev-ref HEAD)
 url="$repoURL/commit/$revision"
+date=$(date)
 mkdir -p tree
-echo "{\"repo\":\"$repo\",\"branch\":\"$branch\",\"url\":\"$url\",\"revision\":\"$revision\"}" > tree/revision.json
+echo "{\"repo\":\"$repo\",\"branch\":\"$branch\",\"url\":\"$url\",\"revision\":\"$revision\", \"date\":\"$date\"}" > tree/revision.json
